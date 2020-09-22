d17d6e85a1d03a1ead23e87cd76a6399b4e7986e
Mark Bort Tomas <maboto01@xuqueralumnat.es>
2020-09-22 18:57:36 +0200

    Finished

---
+++ main.js
@@ -0,0 +1,6 @@
+console.log("Hola mundo!");
+alertMessage = "Soy el primer script";
+const ALERT_MESSAGE = alertMessage.concat(" y estoy funcionando sobre ", navigator.appVersion);
+alert(ALERT_MESSAGE);
+
+
