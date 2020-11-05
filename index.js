cb7b8a678e59f97bbc2d948742c3a3a912fa9383
Mark <bortmarc8@gmail.com>
2020-11-05 22:59:42 +0100

    Finished AE3

--- index.js
+++ index.js
@@ -2,38 +2,47 @@ let pageCounter = 1;
 let callBack;
 
 document.addEventListener('DOMContentLoaded', () => {
-    nextDisabled();
-    backDisabled();
+    btnNextSwitch(false);
+    btnBackSwitch(false);
     eventListeners();
 });
 
-const nextDisabled = () => {
-    btnNext.disabled = true;
-    btnNext.style.background = 'grey';
+const btnNextSwitch = (latch) => {
+    if (latch) {
+        btnNext.disabled = false;
+        btnNext.style.background = 'blue';
+    }else{
+        btnNext.disabled = true;
+        btnNext.style.background = 'grey';
+    }
 }
 
-const nextEnabled = () => {
-    btnNext.disabled = false;
-    btnNext.style.background = 'blue';
-}
-
-const backDisabled = () => {
-    btnBack.disabled = true;
-    btnBack.style.background = 'grey';
-}
-
-const backEnabled = () => {
-    btnBack.disabled = false;
-    btnBack.style.background = 'lightsalmon';
+const btnBackSwitch = (latch) => {
+    if (latch) {
+        btnBack.disabled = false;
+        btnBack.style.background = 'lightsalmon';
+    }else{
+        btnBack.disabled = true;
+        btnBack.style.background = 'grey';
+    }
 }
 
 const eventListeners = () => {
     const btnNext = document.getElementById('btnNext');
     const btnBack = document.getElementById('btnBack');
     const cbURL = document.getElementById('callBackURL');
+    const tbNameSurname = document.getElementById('tbNameSurname');
+    const birthDate = document.getElementById('birthDate');
+    const postalCode = document.getElementById('postalCode');
+    const textTown = document.getElementById('textTown');
+    const generalError = document.getElementById('generalError');
+    const tbNameSurnameError = document.getElementById('tbNameSurnameError');
+    const birthDateError = document.getElementById('birthDateError');
+    const postalCodeError = document.getElementById('postalCodeError');
+    const textTownError = document.getElementById('textTownError');
 
     cbURL.addEventListener('input', () => {
-        cbURL.value === '' ? nextDisabled() : nextEnabled();
+        cbURL.value === '' ? btnNextSwitch(false) : btnNextSwitch(true);
     });
 
     btnBack.addEventListener('click', (event) => {
@@ -45,41 +54,156 @@ const eventListeners = () => {
         pageCounter++;
         changeScreen(pageCounter);
     });
+
+    tbNameSurname.addEventListener('click', (event) => { //Mejorable
+        tbNameSurname.value === '' ?  tbNameSurnameError.style.display = 'block' : tbNameSurnameError.style.display = 'none';
+        checkScreen2();
+    });
+
+    tbNameSurname.addEventListener('input', (event) => { //Mejorable
+        tbNameSurname.value === '' ?  tbNameSurnameError.style.display = 'block' : tbNameSurnameError.style.display = 'none';
+        checkScreen2();
+    });
+
+    birthDate.addEventListener('click', (event) => { //NO VA
+        birthDate.value === '' ? birthDateError.style.display = 'block' : birthDateError.style.display = 'none';
+        checkScreen2();
+    });
+
+    birthDate.addEventListener('input', (event) => { //NO VA
+        birthDate.value === '' ? birthDateError.style.display = 'block' : birthDateError.style.display = 'none';
+        checkScreen2();
+    });
+
+    postalCode.addEventListener('click', (event) => { //Mejorable
+        postalCode.value.length != 5 ? postalCodeError.style.display = 'block' : postalCodeError.style.display = 'none';
+        checkScreen2();
+    });
+
+    postalCode.addEventListener('input', (event) => { //Mejorable
+        postalCode.value.length != 5 ? postalCodeError.style.display = 'block' : postalCodeError.style.display = 'none';
+        checkScreen2();
+    });
+
+    textTown.addEventListener('click', (event) => { //Mejorable
+        textTown.value === '' ? textTownError.style.display = 'block' : textTownError.style.display = 'none';
+        textTown.value = textTown.value.toUpperCase();
+        checkScreen2();
+    });
+
+    textTown.addEventListener('input', (event) => { //Mejorable
+        textTown.value === '' ? textTownError.style.display = 'block' : textTownError.style.display = 'none';
+        textTown.value = textTown.value.toUpperCase();
+        checkScreen2();
+    });
+
+    const checkScreen2 = () => {
+        if (textTownError.style.display == 'none' && postalCodeError.style.display == 'none' && birthDateError.style.display == 'none' && tbNameSurnameError.style.display == 'none') {
+            btnNextSwitch(true);
+            generalError.style.display = 'none';
+        }else{
+            btnNextSwitch(false);
+            generalError.style.display = 'block';
+            document.getElementById('generalError').innerHTML = '';
+            if ( tbNameSurnameError.style.display == 'block') {document.getElementById('generalError').innerHTML += 'Error en el campo: Nombre y apellidos<br>';}
+            if ( birthDateError.style.display == 'block') {document.getElementById('generalError').innerHTML += 'Error en el campo: Fecha de nacimiento<br>';}
+            if ( postalCodeError.style.display == 'block') {document.getElementById('generalError').innerHTML += 'Error en el campo: Código postal<br>';}
+            if ( textTownError.style.display == 'block') { document.getElementById('generalError').innerHTML += 'Error en el campo: Municipio<br>';}
+        }
+    }
 }
 
 const changeScreen = () => {
     const scr1 = document.getElementById('screen1');
     const scr2 = document.getElementById('screen2');
     const scr3 = document.getElementById('screen3');
-    const scr4 = document.getElementById('screen4');
-    const cbURL = document.getElementById('callBackURL');
 
-    scr1.style.display = "none";
-    scr2.style.display = "none";
-    scr3.style.display = "none";
-    scr4.style.display = "none";
+    scr1.style.display = 'none';
+    scr2.style.display = 'none';
+    scr3.style.display = 'none';
 
-    backEnabled();
+    btnBackSwitch(true);
 
-    switch (pageCounter) {
+    switch (pageCounter) {  
         case 1:
-            scr1.style.display = "block";
-            backDisabled();
+            goToPage1();
             break;
         case 2:
-            scr2.style.display = "block";
-            callBack = cbURL.value;
+            goToPage2();
             break;
         case 3:
-            scr3.style.display = "block";
+            goToPage3();
             break;
         case 4:
-            scr4.style.display = "block";
+            goToPage4();
+            break;
+        case 5:
+            goToPage5();
             break;
         default:
-            scr1.style.display = "block";
             pageCounter = 1;
-            backDisabled();
+            goToPage1();
             break;
     }
+}
+
+const goToPage1 = () => {
+    const scr1 = document.getElementById('screen1');
+    scr1.style.display = 'block';
+    btnBackSwitch(false);
+}
+
+const goToPage2 = () => {
+    const scr2 = document.getElementById('screen2');
+    const cbURL = document.getElementById('callBackURL');
+    const tbNameSurname = document.getElementById('tbNameSurname');
+    const birthDate = document.getElementById('birthDate');
+    const direction = document.getElementById('direction');
+    const postalCode = document.getElementById('postalCode');
+    const textProvince = document.getElementById('textProvince');
+    const textTown = document.getElementById('textTown');
+    document.getElementById('lblHeader').innerHTML = 'Formulario de solicitud de datos';
+    btnNextSwitch(false);
+
+    scr2.style.display = 'block';
+    callBack = cbURL.value;
+    tbNameSurname.disabled = false;
+    birthDate.disabled = false;
+    direction.disabled = false;
+    postalCode.disabled = false;
+    textProvince.disabled = false;
+    textTown.disabled = false;
+}
+
+const goToPage3 = () => {
+    const scr2 = document.getElementById('screen2');
+    const tbNameSurname = document.getElementById('tbNameSurname');
+    const birthDate = document.getElementById('birthDate');
+    const direction = document.getElementById('direction');
+    const postalCode = document.getElementById('postalCode');
+    const textProvince = document.getElementById('textProvince');
+    const textTown = document.getElementById('textTown');
+    document.getElementById('lblHeader').innerHTML = 'Confirmación de datos';
+
+    scr2.style.display = 'block';
+    tbNameSurname.disabled = true;
+    birthDate.disabled = true;
+    direction.disabled = true;
+    postalCode.disabled = true;
+    textProvince.disabled = true;
+    textTown.disabled = true;
+}
+
+const goToPage4 = () => {
+    const scr3 = document.getElementById('screen3');
+    const btnNext = document.getElementById('btnNext');
+    scr3.style.display = 'block';
+    btnBackSwitch(false);
+    btnNext.text = 'Finalizar';
+}
+
+const goToPage5 = () => {
+    btnBackSwitch(false);  
+    btnNextSwitch(false);
+    window.location.replace('http://'+callBack);
 }
